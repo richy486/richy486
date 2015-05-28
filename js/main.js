ef0a3af0dfa72d70de005218b1ec344f3b8191ee
richy486 <richy486@gmail.com>
2015-05-28 16:50:15 +0200

    Updated thumbs to be square.

--- js/main.js
+++ js/main.js
@@ -13,16 +13,28 @@ jQuery(document).ready(function($){
 
 	//open the quick view panel
 	$('.cd-trigger').on('click', function(event){
-		var selectedImage = $(this).parent('.cd-item').children('img')
-			, slectedImageUrl = selectedImage.attr('src');
+		var selectedImage = $(this).parent('.cd-item').children('img');
+			// , slectedImageUrl = selectedImage.attr('src');
         selectedId = $(this).parent('.cd-item').id();
 
 		$('body').addClass('overlay-layer');
 		animateQuickView(selectedImage, sliderFinalWidth, maxQuickWidth, 'open');
 
+        // TODO: fix this
+        // slectedImageUrl = "img/gems-1.png";
+
+        var hashString = "#";
+        var classString = ".cd-quick-view";
+        var identifer = hashString.concat(selectedId, classString);
+        var sliderWrapper = $(identifer).find('.cd-slider-wrapper');
+        var cdSlider = sliderWrapper.find('.cd-slider');
+        var target = cdSlider.children('li');//.find('.selected');
+        var targetImage = target.children('img');
+        var targetImageUrl = targetImage.attr('src');
+
 		//update the visible slider image in the quick view panel
 		//you don't need to implement/use the updateQuickView if retrieving the quick view data with ajax
-		updateQuickView(slectedImageUrl);
+		updateQuickView(targetImageUrl);
 	});
 
 	//close the quick view panel
@@ -79,7 +91,9 @@ jQuery(document).ready(function($){
 			selectedImage = $('.empty-box').find('img');
 		//update the image in the gallery
 		if( !$('.cd-quick-view').hasClass('velocity-animating') && $('.cd-quick-view').hasClass('add-content')) {
-			selectedImage.attr('src', activeSliderUrl);
+
+            // Don't update image
+			// selectedImage.attr('src', activeSliderUrl);
 			animateQuickView(selectedImage, finalWidth, maxQuickWidth, 'close');
 		} else {
 			closeNoAnimation(selectedImage, finalWidth, maxQuickWidth);
@@ -89,33 +103,59 @@ jQuery(document).ready(function($){
 	function animateQuickView(image, finalWidth, maxQuickWidth, animationType) {
 		//store some image data (width, top position, ...)
 		//store window data to calculate quick view panel position
+
+        // <div id="gems" class="cd-quick-view">
+        //     <div class="cd-slider-wrapper">
+        //         <ul class="cd-slider">
+        //             <li class="selected"><img src="img/gems-1.png" alt="Screen shot 1"></li>
+
+        var hashString = "#";
+        var classString = ".cd-quick-view";
+        var identifer = hashString.concat(selectedId, classString);
+
+        // .find('.cd-slider')
+
+        var sliderWrapper = $(identifer).find('.cd-slider-wrapper');
+        var cdSlider = sliderWrapper.find('.cd-slider');
+        var selected = cdSlider.find('.selected');
+        var selectedImage = selected.children('img');
+
+        var identiferItem = $(identifer);
+
+        // image = selectedImage;
+        // var selectedImage $(identifer).find('.cd-slider-wrapper').find('.cd-slider').find('.selected');
+
 		var parentListItem = image.parent('.cd-item'),
-			topSelected = image.offset().top - $(window).scrollTop(),
-			leftSelected = image.offset().left,
-			widthSelected = image.width(),
-			heightSelected = image.height(),
+			selectedTop = image.offset().top - $(window).scrollTop(),
+			selectedLeft = image.offset().left,
+			selectedWidth = image.width(),
+			selectedHeight = selectedWidth,//image.height(),
 			windowWidth = $(window).width(),
 			windowHeight = $(window).height(),
 			finalLeft = (windowWidth - finalWidth)/2,
-			finalHeight = finalWidth * heightSelected/widthSelected,
+			finalHeight = finalWidth * selectedHeight/selectedWidth,
 			finalTop = (windowHeight - Math.max(finalHeight, $(window).height() * 0.9))/2,
 			quickViewWidth = ( windowWidth * .8 < maxQuickWidth ) ? windowWidth * .8 : maxQuickWidth ,
-			quickViewLeft = (windowWidth - quickViewWidth)/2;
-            quickViewHeight = Math.max(finalHeight, $(window).height() * 0.9)
-        var str1 = "#";
-        var str2 = ".cd-quick-view";
-        var str3 = " Have a nice day!";
-        var identifer = str1.concat(selectedId,str2);
+			quickViewLeft = (windowWidth - quickViewWidth)/2,
+            quickViewHeight = $(window).height() * 0.9 //Math.max(finalHeight, $(window).height() * 0.9)
+
+        // selectedHeight = 257;
+        // selectedLeft = 507.6875;
+        // selectedTop = 146;
+        // selectedWidth = 257;
+
+        // image is the thumb
 
 		if( animationType == 'open') {
 			//hide the image in the gallery
 			parentListItem.addClass('empty-box');
+            selectedImage.addClass('empty-box');
 			//place the quick view over the image gallery and give it the dimension of the gallery image
 			$(identifer).css({
-			    "top": topSelected,
-			    "left": leftSelected,
-			    "width": widthSelected,
-                "height": heightSelected,
+			    "top": selectedTop,
+			    "left": selectedLeft,
+			    "width": selectedWidth,
+                "height": selectedHeight,
 			}).velocity({
 				//animate the quick view: animate its width and center it in the viewport
 				//during this animation, only the slider image is visible
@@ -144,10 +184,10 @@ jQuery(document).ready(function($){
 			}, 300, 'ease', function(){
 				$('body').removeClass('overlay-layer');
 				$(identifer).removeClass('animate-width').velocity({
-					"top": topSelected,
-				    "left": leftSelected,
-				    "width": widthSelected,
-                    "height": heightSelected,
+					"top": selectedTop,
+				    "left": selectedLeft,
+				    "width": selectedWidth,
+                    "height": selectedHeight,
 				}, 500, 'ease', function(){
 					$(identifer).removeClass('is-visible');
 					parentListItem.removeClass('empty-box');
@@ -157,17 +197,17 @@ jQuery(document).ready(function($){
 	}
 	function closeNoAnimation(image, finalWidth, maxQuickWidth) {
 		var parentListItem = image.parent('.cd-item'),
-			topSelected = image.offset().top - $(window).scrollTop(),
-			leftSelected = image.offset().left,
-			widthSelected = image.width();
+			selectedTop = image.offset().top - $(window).scrollTop(),
+			selectedLeft = image.offset().left,
+			selectedWidth = image.width();
 
 		//close the quick view reverting the animation
 		$('body').removeClass('overlay-layer');
 		parentListItem.removeClass('empty-box');
 		$('.cd-quick-view').velocity("stop").removeClass('add-content animate-width is-visible').css({
-			"top": topSelected,
-		    "left": leftSelected,
-		    "width": widthSelected,
+			"top": selectedTop,
+		    "left": selectedLeft,
+		    "width": selectedWidth,
 		});
 	}
 });
